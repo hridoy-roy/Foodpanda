ae96d72a8643570cda070205dcb0041618821245
Hridoy Roy <hridayroy05@gmail.com>
2020-12-24 21:18:58 +0600

    Fresh Wordpress Project

---
+++ wp-content/themes/foodpanda/index.php
@@ -0,0 +1,17 @@
+<?php
+/*
+* first main Template // Hridoy roy
+* @pac
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Panda</title>
+</head>
+<body>
+    
+</body>
+</html>
