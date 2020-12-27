12404623a121f1970eaf0a2f8a8b75696dd88147
Hridoy Roy <hridayroy05@gmail.com>
2020-12-27 23:31:46 +0600

    #7 | enqueue scripts

--- wp-content/themes/foodpanda/functions.php
+++ wp-content/themes/foodpanda/functions.php
@@ -10,6 +10,11 @@ get_stylesheet_uri() // direct style.css link;
 function foodpanda_enqueue_secript(){
     wp_enqueue_style('style',get_stylesheet_uri(),'',filemtime( get_template_directory().'/style.css' ),'all' );
 
+    wp_enqueue_script('main-js',get_template_directory_uri().'/assets/main.js','',filemtime( get_template_directory() . '/assets/main.js'), true );
+
+//    wp_register_style('style',get_stylesheet_uri(),'',filemtime( get_template_directory().'/style.css' ),'all' );
+//    wp_enqueue_style('style');
+
 }
 
 add_action('wp_enqueue_scripts','foodpanda_enqueue_secript');
